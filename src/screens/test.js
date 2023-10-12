907b6f66b8914d3f34d219877f9df9ef2428c754
santaclose666 <cuongthinhgl123@gmail.com>
2023-10-12 08:39:02 +0700

    android

--- src/screens/test.js
+++ src/screens/test.js
@@ -15,17 +15,14 @@ const calHeight = height / 4 + 6;
 
 const Rectangle = () => {
   return (
-    <Svg
-      width={scale(350)}
-      height={verticalScale(175)}
-      viewBox={`0 0 ${scale(350)} ${verticalScale(175)}`}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <Path
-        d="M0 0H430V200C267.707 244.024 173.406 244.512 0 200V0Z"
-        fill="#8EE297"
-      />
-    </Svg>
+    <View style={{flex: 1}}>
+      <Svg fill="none" xmlns="http://www.w3.org/2000/svg">
+        <Path
+          d="M0 0H430V200C267.707 244.024 173.406 244.512 0 200V0Z"
+          fill="#8EE297"
+        />
+      </Svg>
+    </View>
   );
 };
 
